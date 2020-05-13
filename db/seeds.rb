6bb30e4cd411d262f26d17694167bd4929f175fa
loic redon <jabemalo30@gmail.com>
2020-05-13 11:57:15 +0200

    prepa exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  post = Post.new(
+    title: Faker::Company.name,
+    url: Faker::Cannabis.cannabinoid_abbreviation,
+    votes: Faker::Number.between(from: 1, to: 10)
+    )
+  post.save
+end
+
